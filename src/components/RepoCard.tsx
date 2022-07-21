79a35791670601f8051e96d538ab5823d9ecbff2
Alexey-Buglak <alexbuglak2007@gmail.com>
2022-07-21 11:36:55 +0200

    upd styles

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -24,14 +24,18 @@ export function RepoCard({ repo }: { repo: IRepo }) {
     };
 
     return (
-        <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
+        <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-neutral-600 transition-all">
             <a href={repo.html_url} target="_blank">
-                <h2 className="text-lg font-bold">{repo.full_name}</h2>
-                <p className="text-sm">
+                <h2 className="text-lg font-bold text-blue-400">
+                    {repo.full_name}
+                </h2>
+                <p className="text-sm text-blue-200">
                     Forks: <span className="font-bold mr-2">{repo.forks}</span>
                     Watchers: <span className="font-bold">{repo.watchers}</span>
                 </p>
-                <p className="text-sm font-thin">{repo?.description}</p>
+                <p className="text-sm font-thin text-blue-100">
+                    {repo?.description}
+                </p>
 
                 {!isFav && (
                     <button
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -5,7 +5,7 @@ export function FavouritesPage() {
     const { favourites } = useAppSelector((state) => state.github);
 
     if (favourites.length === 0)
-        return <p className="text-center">No items.</p>;
+        return <p className="text-center text-xl text-blue-300">No items jet</p>;
 
     return (
         <div className="flex justify-center pt-10 mx-auto max-h-[500px] max-w-[800px]">
